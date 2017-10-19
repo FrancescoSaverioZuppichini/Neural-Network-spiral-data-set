014b8bfa126abb7ce1057b34cb89063e26478fe9
FrancescoSaverioZuppichini <zuppif@usi.ch>
2017-10-19 21:34:00 +0200

    done

--- core/main.py
+++ core/main.py
@@ -5,10 +5,9 @@ import skeleton as sk
 # sk.run_part1()
 # sk.run_part2()
 
+train_X, train_T= sk.twospirals(250, noise=0.6, twist=800)
 
-train_X, train_T= sk.twospirals(250, noise=0.6, twist=300)
-#
-not_used_x, not_used_t, X_test, T_test  = utils.get_train_and_test_data(train_X,train_T,90)
+res  = utils.get_train_and_test_data(train_X,train_T,80)
 
-sk.competition_load_weights_and_evaluate_X_and_T(X_test,T_test)
+sk.competition_load_weights_and_evaluate_X_and_T(res[2],res[3])
 # sk.competition_train_from_scratch(X_test,T_test)
